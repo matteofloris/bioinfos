e65a66ce6ba1bebed9c24a5bb2e93f87410ae665
matteofloris <matteo.floris@gmail.com>
2021-04-15 18:55:44 +0200

    Update variantInfo.R

--- variantInfo.R
+++ variantInfo.R
@@ -6,13 +6,12 @@ cli <- GraphqlClient$new(
   url = "https://genetics-api.opentargets.io/graphql"
 )
 
-a <- read.table("lista", header=F)$V1
+var <- "10_102877751_T_C"
 
-for (var in a){
-  qry <- Query$new()
-  # create query object
-  qry$query("my_query", paste('query {variantInfo(variantId: "', var, '") {id, rsId, idB37, altAllele, gnomadNFE, gnomadFIN}}', sep=""))
-  # run query and format results:
-  res <- fromJSON(cli$exec(qry$queries$my_query), flatten = TRUE)
-  print(paste(res$data$variantInfo$id, res$data$variantInfo$rsId, res$data$variantInfo$idB37, res$data$variantInfo$altAllele, res$data$variantInfo$gnomadNFE, res$data$variantInfo$gnomadFIN, sep=" "))
-}
+qry <- Query$new()
+
+qry$query("my_query", paste('query {variantInfo(variantId: "', var, '") {id, rsId, idB37, altAllele, gnomadNFE, gnomadFIN}}', sep=""))
+
+res <- fromJSON(cli$exec(qry$queries$my_query), flatten = TRUE)
+
+print(paste(res$data$variantInfo$id, res$data$variantInfo$rsId, res$data$variantInfo$idB37, res$data$variantInfo$altAllele, res$data$variantInfo$gnomadNFE, res$data$variantInfo$gnomadFIN, sep=" "))
